9f7bfeb3d86a198ae2ba4198a92326b69e9e5ae7
SSK-0423 <b1900446@planet.kanazawa-it.ac.jp>
2023-12-01 23:29:01 +0900

    再配布禁止のフリー素材を弾くためにgitignore更新

--- src/Game/Player.cpp
+++ src/Game/Player.cpp
@@ -10,18 +10,21 @@ using namespace Framework;
 namespace FlappyBird
 {
 	Player::Player(Framework::Object* owner)
-		: Framework::IComponent(owner), m_jumpVelocity(-5.f)
+		: Framework::IComponent(owner), m_jumpVelocity(-5.f) // 左上原点なのでマイナス
 	{
 		// スプライト追加
-		Sprite* sprite = new Sprite(L"res/tile_64.png");
+		Sprite* sprite = new Sprite(L"res/character_yosei_02_pink.png");
 		m_owner->AddComponent<SpriteRenderer>(m_owner);
 		m_owner->GetComponent<SpriteRenderer>()->SetSprite(sprite);
 		m_owner->GetComponent<SpriteRenderer>()->SetDrawMode(SPRITE_DRAW_MODE::GAMEOBJECT);
 		m_owner->GetComponent<SpriteRenderer>()->SetLayer(static_cast<UINT>(GAME_SCENE_LAYER::GAMEOBJECT));
 
+		Transform2D* transform = m_owner->GetComponent<Transform2D>();
+		transform->scale = { 50.f, 50.f };
+
 		// コライダー追加
 		RectCollider* collider = m_owner->AddComponent<RectCollider>(m_owner);
-		collider->SetRectSize(100.f, 100.f);
+		collider->SetRectSize(transform->scale.x, transform->scale.y);
 
 		// リジッドボディ追加
 		Rigidbody2D* rigidbody = m_owner->AddComponent<Rigidbody2D>(m_owner);
@@ -39,7 +42,6 @@ namespace FlappyBird
 		// マウス左クリックでジャンプ
 		if (InputSystem::GetMouseButtonDown(MOUSECODE::LEFT))
 		{
-			// 左上原点なので、下方向に力を加える
 			m_owner->GetComponent<Rigidbody2D>()->velocity = { 0.f, 0.f };
 			m_owner->GetComponent<Rigidbody2D>()->AddForce({ 0.f, m_jumpVelocity }, FORCE_MODE::VELOCITY);
 		}
